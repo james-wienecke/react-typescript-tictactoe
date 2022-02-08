df016f007614260341e70e78d3c6f5020a62c97a
James Wienecke <james.a.wienecke@gmail.com>
2022-02-08 15:15:56 -0600

    Switched AppProps to an interface and added import statement to Board.

--- src/AppProps.ts
+++ src/AppProps.ts
@@ -1,3 +1,3 @@
-export type AppProps = {
+export default interface AppProps {
     value: number;
 }
--- src/Board.tsx
+++ src/Board.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import AppProps from "./AppProps";
 import './Game.css'
 import Square from "./Square";
 
