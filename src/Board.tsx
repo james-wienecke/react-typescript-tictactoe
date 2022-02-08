1be0cfd9edb60794a3b91bdf58997ef579e4ab7e
James Wienecke <james.a.wienecke@gmail.com>
2022-02-08 14:20:41 -0600

    Working on getting basic mock-up of layout implemented.

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,2 +1,10 @@
 import React from "react";
 import './Game.css'
+import Square from "./Square";
+
+export default function Board() {
+    const renderSquare = (i: number) => {
+        return <Square />
+    }
+
+}
--- src/Game.tsx
+++ src/Game.tsx
@@ -3,7 +3,16 @@ import React from "react";
 import Board from "./Board"
 import Square from "./Square"
 
-
 export default function Game() {
-    return <div />;
+    return (
+        <div className="game">
+            <div className="game-board">
+                <Board />
+            </div>
+            <div className="game-info">
+                <div>{/* status */}</div>
+                <ol>{/* To-do */}</ol>
+            </div>
+        </div>
+    );
 }
