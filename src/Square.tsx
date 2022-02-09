7326b9545e6fb87ff22fee94295096ea39ba611a
James Wienecke <james.a.wienecke@gmail.com>
2022-02-09 13:42:30 -0600

    Fully replaced text content with svg images.

--- src/Square.tsx
+++ src/Square.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import './Game.css';
+import blank from "./assets/blank.svg";
+import x from "./assets/X.svg";
+import o from "./assets/O.svg";
 
 interface SquareProps {
     handleClick: () => void;
@@ -8,10 +11,30 @@ interface SquareProps {
 }
 
 const Square = (props: SquareProps): JSX.Element => {
+    const getImage = (): string => {
+        if (props.player) {
+            return props.player === 'X' ? x : o;
+        } else {
+            return blank;
+        }
+    }
+
+    const getSVG = () : JSX.Element => {
+        switch (props.player) {
+            case 'X':
+                return <img alt={props.player + " space"} src={x} />
+            case 'O':
+                return <img alt={props.player + " space"} src={o}/>
+            default:
+                return <img alt={props.player + " space"} src={blank} />
+        }
+    }
+
     return (
         <div className="d-flex">
-            <button className="square btn btn-outline-dark rounded-0 p-4" onClick={props.handleClick}>
-                {props.player ? props.player : String.fromCharCode(0x2007)}
+            <button className="square btn btn-outline-dark rounded-0" onClick={props.handleClick}>
+                {/*{props.player ? props.player : String.fromCharCode(0x2007)}*/}
+                <img src={getImage()} alt={`${props.player} space`} />
             </button>
         </div>
     );
