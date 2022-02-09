e3ffd95389b13cc51fd541440b52f59bfa421739
James Wienecke <james.a.wienecke@gmail.com>
2022-02-09 14:48:40 -0600

    Implemented an index-based border drawing system for that classic tic-tac-toe grid style.

--- src/Square.tsx
+++ src/Square.tsx
@@ -1,5 +1,6 @@
 import React, {CSSProperties} from "react";
 import Button from "react-bootstrap/Button";
+import * as CSS from "csstype";
 import './Game.css';
 import blank from "./assets/blank.svg";
 import x from "./assets/X.svg";
@@ -21,14 +22,28 @@ const Square = (props: SquareProps): JSX.Element => {
     }
 
     const setStyle = (): CSSProperties => {
-
-        return
+        const style: CSSProperties = {
+            border: "none",
+            borderRight: "none",
+            borderBottom: "none"
+        };
+        // if square has another square to its right, it gets a border there
+        const hasRightSibling = [0, 1, 3, 4, 6, 7];
+        if (hasRightSibling.includes(props.index)) {
+            style.borderRight = "1px solid black";
+        }
+        // if square has another square to its bottom, it gets a border there
+        const hasBottomSibling = [0, 1, 2, 3, 4, 5];
+        if (hasBottomSibling.includes(props.index)) {
+            style.borderBottom = "1px solid black";
+        }
+        return style;
     }
 
     return (
         <button className="square p-1" style={setStyle()} onClick={props.handleClick}>
             {/*{props.player ? props.player : String.fromCharCode(0x2007)}*/}
-            <img src={getImage()} alt={`${props.player} space`} />
+            <img src={getImage()} alt={`${props.player} space #${props.index}`} />
         </button>
     );
 }
